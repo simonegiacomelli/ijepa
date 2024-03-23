46d2ea4848f703d77eea4dd070c1502f491acb29
Simone <simone.giacomelli@gmail.com>
2024-03-23 09:58:33 +0000

    refactoring paths. todo

--- load_encoder.py
+++ load_encoder.py
@@ -1,8 +1,9 @@
 import torch
 
 
-def _load_cpu_vit_huge(patch_size, num_classes, load_path):
+def _load_cpu_vit_huge(patch_size, num_classes, checkpoint):
     from src.models.vision_transformer import vit_huge
+    load_path = './checkpoint/' + checkpoint
     model = vit_huge(patch_size=patch_size, num_classes=num_classes)
     ckpt = torch.load(load_path, map_location=torch.device('cpu'))
     pretrained_dict = ckpt['encoder']
@@ -17,8 +18,8 @@ def _load_cpu_vit_huge(patch_size, num_classes, load_path):
 
 
 def load_cpu_IN22k_vit_h_14_900e():
-    return _load_cpu_vit_huge(14, 22000, '/home/simone/Downloads/ijepa/IN22K-vit.h.14-900e.pth.tar')
+    return _load_cpu_vit_huge(14, 22000, 'IN22K-vit.h.14-900e.pth.tar')
 
 
 def load_cpu_IN1k_vit_h_14_300e():
-    return _load_cpu_vit_huge(14, 1000, '/home/simone/Downloads/ijepa/IN1K-vit.h.14-300e.pth.tar')
+    return _load_cpu_vit_huge(14, 1000, 'IN1K-vit.h.14-300e.pth.tar')
