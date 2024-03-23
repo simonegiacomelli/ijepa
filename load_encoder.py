00fc5b233d3f4490e1d29319b8a9ae2847a7a403
Simone <simone.giacomelli@gmail.com>
2024-03-23 09:54:46 +0000

    refactoring load

--- load_encoder.py
+++ load_encoder.py
@@ -1,7 +1,9 @@
 import torch
 
 
-def device_cpu(model, load_path):
+def _load_cpu_vit_huge(patch_size, num_classes, load_path):
+    from src.models.vision_transformer import vit_huge
+    model = vit_huge(patch_size=patch_size, num_classes=num_classes)
     ckpt = torch.load(load_path, map_location=torch.device('cpu'))
     pretrained_dict = ckpt['encoder']
 
@@ -11,21 +13,12 @@ def device_cpu(model, load_path):
         encoder_mod[new_key] = v
     model.load_state_dict(encoder_mod)
 
-def load_cpu_IN22k_vit_h_14_900e():
-    from src.models.vision_transformer import vit_huge
-
-    model = vit_huge(patch_size=14, num_classes=22000)
-    load_path = '/home/simone/Downloads/ijepa/IN22K-vit.h.14-900e.pth.tar'
-    device_cpu(model, load_path)
     return model
 
 
+def load_cpu_IN22k_vit_h_14_900e():
+    return _load_cpu_vit_huge(14, 22000, '/home/simone/Downloads/ijepa/IN22K-vit.h.14-900e.pth.tar')
+
+
 def load_cpu_IN1k_vit_h_14_300e():
-    from src.models.vision_transformer import vit_huge
-
-    model = vit_huge(patch_size=14, num_classes=1000)
-    load_path = '/home/simone/Downloads/ijepa/IN1K-vit.h.14-300e.pth.tar'
-    device_cpu(model, load_path)
-    print(model)
-
-
+    return _load_cpu_vit_huge(14, 1000, '/home/simone/Downloads/ijepa/IN1K-vit.h.14-300e.pth.tar')
